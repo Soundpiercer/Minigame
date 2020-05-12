9142176473e95c0c26a944aeaf90d9a04d168e68
Sound piercer Official <soundpiercer@gmail.com>
2020-05-13 07:07:32 +0900

    MN-2 Created Title Scene

---
+++ Minigame/Assets/Script/TitleScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minigame
+{
+    public class TitleScene : MonoBehaviour
+    {
+        public void ArcherCat()
+        {
+
+        }
+
+        public void RandomDice()
+        {
+
+        }
+
+        public void Archero()
+        {
+
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
