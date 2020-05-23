277e3c9fb1720c73cb846837da2b1f7ccad5a9b8
Sound piercer Official <soundpiercer@gmail.com>
2020-05-24 08:52:33 +0900

    MN-4 Make local database static class to store game parameters in Random Dice

---
+++ Minigame/Assets/Script/RandomDice/RandomDiceData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RandomDice
+{
+    public static class RandomDiceData
+    {
+        public static Phase[] phases =
+        {
+            new Phase(1, 10, 1.0f, 0.075f),
+            new Phase(2, 15, 0.9f, 0.08f),
+            new Phase(3, 20, 0.8f, 0.085f),
+            new Phase(4, 25, 0.7f, 0.09f),
+            new Phase(5, 30, 0.6f, 0.095f)
+        };
+    }
+}
--- Minigame/Assets/Script/RandomDice/RandomDiceManager.cs
+++ Minigame/Assets/Script/RandomDice/RandomDiceManager.cs
@@ -21,15 +21,9 @@ namespace RandomDice
 
         public RandomDiceController controller;
         public List<EnemyDiceBehaviour> enemies = new List<EnemyDiceBehaviour>();
+
         [HideInInspector]
-        public Phase[] phases =
-        {
-            new Phase(1, 10, 1.0f, 0.075f),
-            new Phase(2, 15, 0.9f, 0.08f),
-            new Phase(3, 20, 0.8f, 0.085f),
-            new Phase(4, 25, 0.7f, 0.09f),
-            new Phase(5, 30, 0.6f, 0.095f)
-        };
+        public Phase[] phases = RandomDiceData.phases;
 
         private int sp;
         public int SP
