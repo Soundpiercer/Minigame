a79fc48a0dc8d27e5846143a6022baa1e4a2c0e6
Sound piercer Official <soundpiercer@gmail.com>
2020-05-27 03:18:33 +0900

    Refactoring : remove magic numbers from RandomDiceManager

--- Minigame/Assets/Script/RandomDice/RandomDiceManager.cs
+++ Minigame/Assets/Script/RandomDice/RandomDiceManager.cs
@@ -26,33 +26,39 @@ namespace RandomDice
         public List<ShooterDiceBehaviour> shooters = new List<ShooterDiceBehaviour>();
         public Dictionary<ShooterDiceType, int> shooterLevels = new Dictionary<ShooterDiceType, int>();
         public List<EnemyDiceBehaviour> enemies = new List<EnemyDiceBehaviour>();
-        
+
+        private const int INIT_SP = 100;
+        private const int INIT_REQUIRED_SP_TO_SPAWN = 10;
+        private const int MINIMUM_LEVEL = 1;
+        private const int MAXIMUM_LEVEL = 5;
+        private const int REQUIRED_SP_TO_LEVELUP = 100;
+
         public void Init()
         {
             phases = RandomDiceData.phases;
             foreach (string s in Enum.GetNames(typeof(ShooterDiceType)))
             {
-                shooterLevels.Add((ShooterDiceType)Enum.Parse(typeof(ShooterDiceType), s), 1);
+                shooterLevels.Add((ShooterDiceType)Enum.Parse(typeof(ShooterDiceType), s), MINIMUM_LEVEL);
             }
 
-            SP = 100;
-            RequiredSPToSpawn = 10;
+            SP = INIT_SP;
+            RequiredSPToSpawn = INIT_REQUIRED_SP_TO_SPAWN;
         }
 
         public void DiceLevelUp(ShooterDiceType diceType)
         {
             // if the dice already reached the maximum level, return
-            if (shooterLevels[diceType] == 5)
+            if (shooterLevels[diceType] == MAXIMUM_LEVEL)
                 return;
 
             // Minimum required SP is 100
-            if (SP < 100)
+            if (SP < REQUIRED_SP_TO_LEVELUP)
                 return;
 
-            SP -= 100;
+            SP -= REQUIRED_SP_TO_LEVELUP;
 
             // Level Up to managed data
-            shooterLevels[diceType] = Mathf.Clamp(shooterLevels[diceType] + 1, 1, 5);
+            shooterLevels[diceType] = Mathf.Clamp(shooterLevels[diceType] + 1, MINIMUM_LEVEL, MAXIMUM_LEVEL);
 
             // Level Up currently active shooter dice
             foreach (ShooterDiceBehaviour shooter in shooters)
